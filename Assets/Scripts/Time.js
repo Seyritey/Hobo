396699ea30cb11cb87c62025ba6e1f867f379b4e
Ilya <seyritey777@gmail.com>
2015-08-03 23:49:16 +0300

    TimeFix

--- Assets/Scripts/Time.js
+++ Assets/Scripts/Time.js
@@ -1,51 +1,62 @@
 #pragma strict
-var num : int = 0;
-var sprite1 : Sprite;
-var sprite2 : Sprite;
-var temp : float = 0;
-var night : int = 0;
-var dayTime : int = 0;
-var nightTime : int = 0;
+var num: int = 0;
+var num2: int = 0; 
+var sprite1: Sprite;
+var sprite2: Sprite;
+var temp: float = 0;
+var night: int = 0;
+var day: int = 0;
+var nightTime: int = 0;
 
 
 function Start() {
-   StartCoroutine("timeChange");
+	StartCoroutine("timeChange");
 }
- 
-function timeChange2() {     
-	 //var temp : int = UnityEngine.Time.time;
-     if (Input.GetMouseButtonDown (0)) {
 
-			var hit : RaycastHit;
+function timeChange2() {
+	//var temp : int = UnityEngine.Time.time;
+	if (Input.GetMouseButtonDown(0)) {
 
-			var ray : Ray;
-			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			if(Physics.Raycast (ray, hit, 100)){
-
-				if(hit.transform.tag == "Player"){
-					print(dayTime);
-				}
+		if (num2 == 0) {
+			StopCoroutine("timeChange");
+			num2++;
+		} else {
+			num2--;
+			StartCoroutine("timeChange");
 		}
-     
+
+		var hit: RaycastHit;
+
+		var ray: Ray;
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if (Physics.Raycast(ray, hit, 100)) {
+
+			if (hit.transform.tag == "Player") {
+				temp = UnityEngine.Time.time - (day * 2);
+				print(temp);
+			}
+		}
+
 	}
 }
+
 function timeChange() {
- yield WaitForSeconds(2);
- //temp = UnityEngine.Time.time - temp;
- //print(temp);
- 
-    if (num == 0) {
-  GetComponent(SpriteRenderer).sprite = sprite1;
-  num++;
- } else {
-  num--;
-  GetComponent(SpriteRenderer).sprite = sprite2;
- }
- 	dayTime*2;
-     StartCoroutine("timeChange");
+	yield WaitForSeconds(2);
+	temp = UnityEngine.Time.time - temp;
+
+	if (num == 0) {
+		GetComponent(SpriteRenderer).sprite = sprite1;
+		num++;
+	} else {
+		num--;
+		GetComponent(SpriteRenderer).sprite = sprite2;
+	}
+	day++;
+	StartCoroutine("timeChange");
 }
 
-function Update () {
+function Update() {
 	timeChange2();
 }
