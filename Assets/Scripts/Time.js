573d43e8a1d0270921aa3e107221bd1606534eac
EmptySpace <3d99c4ok@mail.ru>
2015-08-03 23:39:26 +0300

    Time
    TimeChange

--- Assets/Scripts/Time.js
+++ Assets/Scripts/Time.js
@@ -2,29 +2,50 @@
 var num : int = 0;
 var sprite1 : Sprite;
 var sprite2 : Sprite;
-//var temp : float = 0;
-
+var temp : float = 0;
+var night : int = 0;
+var dayTime : int = 0;
+var nightTime : int = 0;
 
 
 function Start() {
    StartCoroutine("timeChange");
 }
  
-function timeChange() {
-	yield WaitForSeconds(2);
-	//temp = UnityEngine.Time.time - temp;
-	//print(temp);
-	
-    if (num == 0) {
-		GetComponent(SpriteRenderer).sprite = sprite1;
-		num++;
-	} else {
-		num--;
-		GetComponent(SpriteRenderer).sprite = sprite2;
+function timeChange2() {     
+	 //var temp : int = UnityEngine.Time.time;
+     if (Input.GetMouseButtonDown (0)) {
+
+			var hit : RaycastHit;
+
+			var ray : Ray;
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if(Physics.Raycast (ray, hit, 100)){
+
+				if(hit.transform.tag == "Player"){
+					print(dayTime);
+				}
+		}
+     
 	}
+}
+function timeChange() {
+ yield WaitForSeconds(2);
+ //temp = UnityEngine.Time.time - temp;
+ //print(temp);
+ 
+    if (num == 0) {
+  GetComponent(SpriteRenderer).sprite = sprite1;
+  num++;
+ } else {
+  num--;
+  GetComponent(SpriteRenderer).sprite = sprite2;
+ }
+ 	dayTime*2;
      StartCoroutine("timeChange");
 }
 
 function Update () {
-	
+	timeChange2();
 }
